be96293e4795d82e07c860146326705141a61f38
Mehrshad <mehrshad.farahani.jafari@gmail.com>
2024-01-15 21:17:49 +0330

    initial

--- Models/Artist.cs
+++
@@ -1,9 +0,0 @@
-#nullable disable
-namespace Spotify.Models;
-
-public class Artist
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<Music> Musics { get; set; }
-}
