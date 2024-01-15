97b4f6dd6f706dfcaf46fd904df2c261683cee51
Mehrshad <mehrshad.farahani.jafari@gmail.com>
2024-01-15 21:26:56 +0330

    [Add] Home Controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Spotify.Models;
+using Microsoft.AspNetCore.Mvc;
+using Spotify.Data.Repositories.Contracts;
+using Spotify.Models.DTOs;
+namespace Spotify.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IMusicRepository _musicRepository;
+
+    public HomeController(IMusicRepository musicRepository)
+    {
+        _musicRepository = musicRepository;    
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        IReadOnlyList<Music> musics = await _musicRepository.GetAllMusicsWithArtist();
+        ShowMusicDto smd = new() { Musics = musics };
+        return View(smd);
+    }
+
+    public IActionResult Error()
+    {
+        return View("_Error");
+    }
+}
